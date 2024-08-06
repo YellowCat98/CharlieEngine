81162f0d07ac1e9cc2c4a0dcdb0c60715d1f310f
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-06 19:17:21 +0100

    lets try this

--- main.hpp
+++ main.hpp
@@ -1,4 +1,6 @@
+#pragma once
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
 @interface CharlieEngineInject : NSObject
 + (void)loadLibrary;
 @end
--- main.mm
+++ main.mm
@@ -1,11 +1,22 @@
 #import "main.hpp"
 #import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
 
 @implementation CharlieEngineInject : NSObject
 
 + (void)load {
-    [self loadLibrary];
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"hello"
+                                                                              message:@"yo!"
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+      
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"go away"
+                                                      style:UIAlertActionStyleDefault
+                                                    handler:nil];
+      
+    [alertController addAction:okAction];
+
+    UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+
+    [rootViewController presentViewController:alertController animated:YES completion:nil];
 }
 
 + (void)loadLibrary {
