e2a196e2c78faf0e5e9b19ffb4e7922d6243deb3
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-06 19:37:53 +0100

    hmm

--- main.mm
+++ main.mm
@@ -2,39 +2,32 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-@implementation CharlieEngineInject : NSObject
+@interface CharlieEngineInject : NSObject
+@end
+
+@implementation CharlieEngineInject
 
 + (void)load {
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"hello"
-                                                                              message:@"yo!"
-                                                                      preferredStyle:UIAlertControllerStyleAlert];
-      
-    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"go away"
-                                                      style:UIAlertActionStyleDefault
-                                                    handler:nil];
-      
-    [alertController addAction:okAction];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        // Ensure UI-related code runs on the main thread
 
-    UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Hello"
+                                                                                  message:@"Yo!"
+                                                                           preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Go away"
+                                                            style:UIAlertActionStyleDefault
+                                                          handler:nil];
+        
+        [alertController addAction:okAction];
 
-    [rootViewController presentViewController:alertController animated:YES completion:nil];
+        UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+        UIViewController *rootViewController = keyWindow.rootViewController;
+
+        if (rootViewController.presentedViewController == nil) {
+            [rootViewController presentViewController:alertController animated:YES completion:nil];
+        }
+    });
 }
-
-+ (void)loadLibrary {
-      
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"hello"
-                                                                              message:@"yo!"
-                                                                      preferredStyle:UIAlertControllerStyleAlert];
-      
-    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"go away"
-                                                      style:UIAlertActionStyleDefault
-                                                    handler:nil];
-      
-    [alertController addAction:okAction];
-
-    UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-
-    [rootViewController presentViewController:alertController animated:YES completion:nil];
-  }
 
 @end
