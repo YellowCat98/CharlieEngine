7412a9e96050a2ca667b074fe7d0d36536f2caf7
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-17 17:47:41 +0100

    remove test funtions cause were very very useless i will bring back writetolog soon cause i need

--- inject/main.mm
+++ inject/main.mm
@@ -2,58 +2,14 @@
 #import <Foundation/Foundation.h>
 
 @interface CharlieEngineInject : NSObject
-+ (void)initializeInjection;
-+ (void)writeToLog:(NSString *)message;
 @end
 
 @implementation CharlieEngineInject
-
-+ (void)initializeInjection {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Hello"
-                                                                                  message:@"Yo!"
-                                                                           preferredStyle:UIAlertControllerStyleAlert];
-
-        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Go away"
-                                                           style:UIAlertActionStyleDefault
-                                                         handler:nil];
-
-        [alertController addAction:okAction];
-
-        UIWindow *keyWindow = [UIApplication.sharedApplication.windows firstObject];
-        UIViewController *rootViewController = keyWindow.rootViewController;
-
-        if (rootViewController && rootViewController.presentedViewController == nil) {
-            [rootViewController presentViewController:alertController animated:YES completion:nil];
-        } else {
-            [self writeToLog:@"Failed to present alert: rootViewController is nil or already presenting another view controller."];
-        }
-    });
-}
-
-+ (void)writeToLog:(NSString *)message {
-    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    NSString *logFilePath = [documentsPath stringByAppendingPathComponent:@"CharlieEngineInject.log"];
-
-    NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:logFilePath];
-    if (!fileHandle) {
-        [[NSFileManager defaultManager] createFileAtPath:logFilePath contents:nil attributes:nil];
-        fileHandle = [NSFileHandle fileHandleForWritingAtPath:logFilePath];
-    }
-
-    [fileHandle seekToEndOfFile];
-    NSString *logMessage = [NSString stringWithFormat:@"%@\n", message];
-    [fileHandle writeData:[logMessage dataUsingEncoding:NSUTF8StringEncoding]];
-    [fileHandle closeFile];
-}
-
 @end
 
 __attribute__((constructor))
 static void initialize() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         NSLog(@"Hello from CharlieEngine");
-        [CharlieEngineInject writeToLog:@"Delayed initialization"];
-        //[CharlieEngineInject initializeInjection];
     });
 }
