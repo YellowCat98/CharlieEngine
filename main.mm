c6ee59412bfe3da00eab29584fcef499db9845ca
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-17 15:09:43 +0100

    try to see where its crashing

--- main.mm
+++ main.mm
@@ -54,6 +54,6 @@ static void initialize() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         NSLog(@"Hello from CharlieEngine");
         [CharlieEngineInject writeToLog:@"Delayed initialization"];
-        [CharlieEngineInject initializeInjection];
+        //[CharlieEngineInject initializeInjection];
     });
 }
