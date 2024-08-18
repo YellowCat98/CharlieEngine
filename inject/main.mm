eef10c8b1ea4a9a2fa212981e8c390158f2a9f2f
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-18 23:00:39 +0100

    code cleanup

--- inject/main.mm
+++ inject/main.mm
@@ -10,17 +10,15 @@
 
 __attribute__((constructor))
 static void initialize() {
-    //dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		NSString *str = @"hi";
+	NSString *str = @"hi";
 
-		NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-		NSString *documentsDirectory = [paths objectAtIndex:0];
-		NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"hi.txt"];
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *documentsDirectory = [paths objectAtIndex:0];
+	NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"hi.txt"];
 
-		NSError *error;
-		BOOL success = [str writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
-		if (!success) {
-			NSLog(@"Error writing file: %@", [error localizedDescription]);
-		}
-   // });
+	NSError *error;
+	BOOL success = [str writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
+	if (!success) {
+		NSLog(@"Error writing file: %@", [error localizedDescription]);
+	}
 }
