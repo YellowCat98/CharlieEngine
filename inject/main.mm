66fc26d0e400467af409e877aefb216ef7384a10
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-18 22:36:26 +0100

    lets try that instead

--- inject/main.mm
+++ inject/main.mm
@@ -6,64 +6,21 @@
 @end
 
 @implementation CharlieEngineInject
-
-void showAlert(NSString *tiddies, NSString *meows, NSString *gyatt) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
-        if (!keyWindow) {
-            NSLog(@"Error: keyWindow is nil");
-            return;
-        }
-        
-        UIViewController *currentFucker = keyWindow.rootViewController;
-        if (!currentFucker) {
-            NSLog(@"Error: rootViewController is nil");
-            return;
-        }
-
-        while (currentFucker.presentedViewController) {
-            currentFucker = currentFucker.presentedViewController;
-        }
-
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:tiddies message:meows preferredStyle:UIAlertControllerStyleAlert];
-
-        UIAlertAction *fuckoff = [UIAlertAction actionWithTitle:gyatt style:UIAlertActionStyleDefault handler:nil];
-        [alert addAction:fuckoff];
-
-        [currentFucker presentViewController:alert animated:YES completion:nil];
-    });
-}
-
-// Function to write data to a file in the Documents directory
-void writeFileToDocumentsDirectory(const std::string& fileName, const std::string& content) {
-    // Convert std::string to NSString
-    NSString *fileNameNSString = [NSString stringWithUTF8String:fileName.c_str()];
-    NSString *contentNSString = [NSString stringWithUTF8String:content.c_str()];
-
-    // Get the path to the Documents directory
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    
-    // Create the full file path
-    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:fileNameNSString];
-    
-    // Write the content to the file
-    NSError *error = nil;
-    BOOL success = [contentNSString writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
-    
-    if (!success) {
-        NSLog(@"Failed to write file: %@", [error localizedDescription]);
-    } else {
-        NSLog(@"File successfully written to %@", filePath);
-    }
-}
-
-
 @end
 
 __attribute__((constructor))
 static void initialize() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        writeFileToDocumentsDirectory("test.txt", "HI");
+		NSString *str = @"hi";
+
+		NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+		NSString *documentsDirectory = [paths objectAtIndex:0];
+		NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"hi.txt"];
+
+		NSError *error;
+		BOOL success = [str writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
+		if (!success) {
+			NSLog(@"Error writing file: %@", [error localizedDescription]);
+		}
     });
 }
