c6c139f4c35bd7e4cf3dcb6c8625d869037bb890
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-19 19:14:35 +0100

    ok lets try if this one compiles!

--- inject/main.mm
+++ inject/main.mm
@@ -1,24 +1,37 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #include <string>
-
-//@interface CharlieEngineInject : NSObject
-//@end
-
-//@implementation CharlieEngineInject
-//@end
+#include <dlfcn.h>
 
 __attribute__((constructor))
 static void initialize() {
-	NSString *str = @"hi";
-
 	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
 	NSString *documentsDirectory = [paths objectAtIndex:0];
-	NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"hi.txt"];
 
-	NSError *error;
-	BOOL success = [str writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
-	if (!success) {
-		NSLog(@"Error writing file: %@", [error localizedDescription]);
+	NSString* dylibPath = [documentsDirectory stringByAppendingPathComponent:@"libCharlieEngineLoader.dylib"];
+
+	if ([[NSFileManager defaultManager] fileExistsAtPath:dylibPath]) {
+		void *handle = dlopen(dylibPath, RTLD_LAZY); // what does that last arg do
+		if (!handle) {
+			NSLog(@"Error loading libCharlieEngineLoader.dylib: %@", dlerror());
+			NSString* errorPath = [documentsDirector stringByAppendingPathComponent:@"last_err.txt"];
+			NSError* error;
+			BOOL success = [str writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
+			if (!success) {
+				NSLog(@"Error writing Error.");
+			}
+		}
+	} else {
+			NSString *str = @"PLACE libCharlieEngineLoader.dylib HERE";
+
+			NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"PLACE libCharlieEngineLoader.dylib HERE"];
+
+			NSError *error;
+			BOOL success = [str writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
+			if (!success) {
+				NSLog(@"Error writing file: %@", [error localizedDescription]);
+			}
 	}
+
+
 }
