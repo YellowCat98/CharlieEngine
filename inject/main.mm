224751a88085c5c818bc5be222a6bae04120359a
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-19 20:18:24 +0100

    fix errors

--- inject/main.mm
+++ inject/main.mm
@@ -13,10 +13,12 @@ static void initialize() {
 	if ([[NSFileManager defaultManager] fileExistsAtPath:dylibPath]) {
 		void *handle = dlopen(dylibPath, RTLD_LAZY); // what does that last arg do
 		if (!handle) {
-			NSLog(@"Error loading libCharlieEngineLoader.dylib: %@", dlerror());
-			NSString* errorPath = [documentsDirector stringByAppendingPathComponent:@"last_err.txt"];
+			NSLog(@"Error loading libCharlieEngineLoader.dylib: %s", dlerror());
+			NSString* errorPath = [documentsDirectory stringByAppendingPathComponent:@"last_err.txt"];
 			NSError* error;
-			BOOL success = [str writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
+			//NSString* errorString = dlerror();
+			NSString* errorString = [NSString stringWithFormat:@"Error loading libCharlieEngineLoader.dylib: %s", dlerror()];
+			BOOL success = [errorString writeToFile:errorPath atomically:YES encoding:NSUTF8StringEncoding error:&error];
 			if (!success) {
 				NSLog(@"Error writing Error.");
 			}
