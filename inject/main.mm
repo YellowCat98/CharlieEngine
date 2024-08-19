fa43fabbb19810248628189b15b4273e8c20565d
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-19 21:34:38 +0100

    ok its time

--- inject/main.mm
+++ inject/main.mm
@@ -33,44 +33,5 @@ static void initialize() {
 			if (!success) {
 				NSLog(@"Error writing file: %@", [error localizedDescription]);
 			}
-	}	
-		// why thgis is so stupid
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"skibidi"
-                                                                       message:@"hi"
-                                                                preferredStyle:UIAlertControllerStyleAlert];
-
-        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"HI"
-                                                               style:UIAlertActionStyleCancel
-                                                             handler:nil];
-        UIAlertAction *otherAction = [UIAlertAction actionWithTitle:@"go away"
-                                                              style:UIAlertActionStyleDefault
-                                                            handler:nil];
-
-        [alert addAction:cancelAction];
-        [alert addAction:otherAction];
-
-        // Ensure there is a key window and root view controller
-        UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
-        if (!keyWindow) {
-            NSLog(@"No key window available");
-            return;
-        }
-
-        UIViewController *rootViewController = keyWindow.rootViewController;
-        if (!rootViewController) {
-            NSLog(@"No root view controller available");
-            return;
-        }
-
-        // Traverse to the top-most presented view controller
-        while (rootViewController.presentedViewController) {
-            rootViewController = rootViewController.presentedViewController;
-        }
-
-        // Present the alert
-        [rootViewController presentViewController:alert animated:YES completion:^{
-            NSLog(@"Alert presented successfully");
-        }];
-    });
+    }
 }
