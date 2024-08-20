402465522e01f0ff4c710f67049b4a778a9ffc23
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-20 13:51:10 +0100

    wtf

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -11,12 +11,12 @@ namespace CharlieEngine {
             NSString* logPath = [documentsDirectory stringByAppendingPathComponent:logName];
             NSError *error = nil;
 
-            NSString *wholeLog [NSString stringWithContentsOfFile:logPath,
-                                                        encoding:NSUTF8StringEncoding,
+            NSString *wholeLog [NSString stringWithContentsOfFile:logPath
+                                                        encoding:NSUTF8StringEncoding
                                                         error:&error];
             NSString *newLog = [wholeLog stringByAppendingString:str];
             NSError* idk;
-            BOOL success = [newLog writeToFile:logPath, atomically:YES encoding:NSUTF8StringEncoding error:&idk];
+            BOOL success = [newLog writeToFile:logPath atomically:YES encoding:NSUTF8StringEncoding error:&idk];
 
             return str;
         };
