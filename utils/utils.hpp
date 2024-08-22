f056d886c3d6ebc25fdfac981be14127e6101244
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-22 15:12:18 +0100

    try that

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -13,18 +13,28 @@ extern "C" int csops(pid_t pid, int ops, void *useraddr, size_t usersize);
 namespace CharlieEngine {
     namespace utils {
         NSString *log(NSString *str, NSString* logName) {
+            // Get the path to the Documents directory
             NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
             NSString *documentsDirectory = [paths objectAtIndex:0];
 
+            // Create the full path to the log file
             NSString* logPath = [documentsDirectory stringByAppendingPathComponent:logName];
             NSError *error = nil;
 
-            NSString *wholeLog = [NSString stringWithContentsOfFile:logPath
-                                                        encoding:NSUTF8StringEncoding
-                                                        error:&error];
+            // Read the current content of the log file, if it exists
+            NSString *wholeLog = [NSString stringWithContentsOfFile:logPath encoding:NSUTF8StringEncoding error:&error];
+            if (error) {
+                NSLog(@"Error reading log file: %@", error.localizedDescription);
+                wholeLog = @""; // Initialize to empty string if file doesn't exist
+            }
+
+            // Append the new log entry
             NSString *newLog = [wholeLog stringByAppendingString:str];
-            NSError* idk;
-            BOOL success = [newLog writeToFile:logPath atomically:YES encoding:NSUTF8StringEncoding error:&idk];
+            BOOL success = [newLog writeToFile:logPath atomically:YES encoding:NSUTF8StringEncoding error:&error];
+            
+            if (!success) {
+                NSLog(@"Error writing to log file: %@", error.localizedDescription);
+            }
 
             return str;
         }
