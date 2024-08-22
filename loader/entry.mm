e87cb2ecd52d7add1ef79c6698c83f6fb1c914a3
YellowCat98 <elcucuyisgay@gmail.com>
2024-08-22 21:49:31 +0100

    change up CharlieEngineLoader a bit to test if it actually loads or not

--- loader/entry.mm
+++ loader/entry.mm
@@ -6,5 +6,8 @@ using namespace CharlieEngine;
 
 __attribute__((constructor))
 static void initialize() {
-    utils::log(@"Hello!", @"CharlieEngineLoader.log");
+    NSURL *url = [NSURL URLWithString:@"https://google.com"];
+    if ([[UIApplication sharedApplication] canOpenURL:url]) {
+        [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+    }
 }
